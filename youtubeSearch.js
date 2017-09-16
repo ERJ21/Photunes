75f8910cac79692d0d4a28800fc0b2c35254077d
Eric Johnson <johnson.r.eric21@gmail.com>
2017-09-16 15:51:54 -0400

    add

---
+++ youtubeSearch.js
@@ -0,0 +1,22 @@
+function getData(query,x) { //query, number of videos
+  var data = $.get(
+    'https://www.googleapis.com/youtube/v3/search',
+    {
+      part:'id',
+      q: query,
+      type: 'video',
+      maxResults: 5,
+      order: 'viewCount',
+      topicId: '/m/04rlf',
+      key: 'AIzaSyCofayi2gIrdRUYDf-vMEtGcbhOGOytw6c'
+    });
+  return data;
+}
+
+function getVideoId(data, x) {//list of videos, index
+  return data.items(x).id.videoId;
+}
+
+function getSrcUrl(videoId) {
+  return
+}
