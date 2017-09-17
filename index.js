217a007b858e214be9e9082279d8d6524cd95146
C.J. Enright <cje@aaichicago.com>
2017-09-16 21:55:29 -0400

    tsalsdjf

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function doTheThing(url, num, res) {
 				doTheThing(url, num+1, res);
 			}
 			else {
-				console.log(data.length)
+				console.log(JSON.stringify(str))
 				console.log('responding to ' + url + ' with ' + data);
 				console.log(data.toString());
 				res.json(data.toString())
